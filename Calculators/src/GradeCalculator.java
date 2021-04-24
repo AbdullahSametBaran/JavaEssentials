553e04ae51290b16f4775b1187b9c4c2657735f1
Abdullah Samet Baran <abdullahsametbaran@gmail.com>
2021-04-24 04:31:45 +0300

    Add files via upload
    Calculators file include two format of calculator which are numerical operations and teacher's grade operator.

---
+++ Calculators/src/GradeCalculator.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GradeCalculator {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int sum = 0, mgrade = 0, fgrade = 0;
+		double avg = 0;
+		int x = 1;
+		final int studentCount = 5;
+
+		for (int k = 0; k < studentCount - 1; k++) {
+
+			for (int i = 1; i < 3; i++) { // For mid-term which affects %30
+
+				System.out.println("Please enter " + x + ". student " + i + ". midterm grade: ");
+
+				mgrade = sc.nextInt();
+
+				if (mgrade > -1 && mgrade <= 100) {
+					sum += mgrade * 1 / 4;
+
+				} else
+					System.out.println("Invalid input." + i--);
+
+			}
+
+			System.out.println("Enter the final grade: ");
+			fgrade = sc.nextInt();
+			sum += fgrade * 1 / 2;
+			x++;
+
+			System.out.println("The " + k + ". student total grade: " + sum);
+			sum = 0;
+		}
+	}
+
+}
